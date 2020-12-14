3732c96368cfe864381fb646d977646f812a13bf
Jesse Walling <jessephilipwalling@gmail.com>
2020-12-14 13:39:29 -0800

    added bitcoin and short functionality, changed formatting

--- stock.py
+++ stock.py
@@ -1,4 +1,5 @@
 import time
+import urllib.parse, urllib.request, urllib.error, json
 from requests_html import HTMLSession
 session = HTMLSession()
 
@@ -27,13 +28,14 @@ class Stock:
         return self.roi() < other.roi()
     
     def __str__(self):
-        return "{} \x1b[0m{}{:^8}\x1b[0m {:^7} {:^9} {:^9} {:^12.2f} {:^12.2f} {}{}%\x1b[0m".format(
+        mult = self.multiplier()
+        return "{} \x1b[0m{}{:^8}\x1b[0m {:^7} {:^12} {:^12} {:^12.2f} {:^12.2f} {}{}%\x1b[0m".format(
             self.id, self.symbolColor(), self.symbol, 
-            self.shares, 
+            str(int(mult) * int(self.shares)), 
             self.buy_price, 
             self.current_price,
-            int(self.shares) * float(self.buy_price),#expense,
-            (int(self.shares) * float(self.current_price)) - (int(self.shares) * float(self.buy_price)),#profit,
+            abs(self.expense()),
+            self.profit(),
             self.roiColor(), self.roi()
             )
     
@@ -53,34 +55,34 @@ class Stock:
             return "\x1b[0;32m" #green
     
     def refresh(self):
-        if self.symbol != 'BTC':
+        quote = 0.0
+        if self.symbol != 'BTC' and self.symbol != 'btc':
             url = "https://old.nasdaq.com/symbol/{}/real-time".format(self.symbol)
             page = session.get(url)
             quote = page.html.find("div#qwidget_lastsale.qwidget-dollar")[0].text[1:]
-            self.current_price = quote
-            return quote
         else:
-            # TODO: Add coindesk api for price scraping
-            self.current_price = 0.0
-            return 0.0
+            btc_str = urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json")
+            btc_data = json.load(btc_str)
+            quote = btc_data["bpi"]["USD"]["rate_float"]
+        self.current_price = float(quote)
+        return float(quote)
     
     def roi(self):
-        recent = 0
-        if self.sold:
-            recent = float(self.sell_price)
-        else:
-            recent = float(self.current_price)
-        profit = (recent - float(self.buy_price)) * int(self.shares)
-        expense = float(self.buy_price) * int(self.shares)
-        roi_ret = (profit / expense) * 100
+        roi_ret = (float(self.profit()) / float(abs(self.expense()))) * 100
         return round(roi_ret, 3)
     
     def expense(self):
-        return float(self.buy_price) * int(self.shares)
+        return (float(self.buy_price) * int(self.multiplier()) * float(self.shares))
     
     def profit(self):
-        return (float(self.current_price) * int(self.shares)) - self.expense()
+        return (float(self.current_price) * int(self.multiplier()) * float(self.shares)) - float(self.expense())
     
     def sell(self):
         self.sell_price = self.current_price
         self.sold = True
+    
+    def multiplier(self):
+        if self.short == True:
+            return -1
+        else:
+            return 1
